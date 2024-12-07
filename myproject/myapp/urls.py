4490330000c019e2fe296f3e43779295af45d08f
DezerdGun <mir6raid@gmail.com>
2024-12-07 20:01:13 +0500

    added first get api

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from .views import ProductListAPIView
 
 
 schema_view = get_schema_view(
@@ -23,4 +24,6 @@ urlpatterns = [
     path('',views.index,name='index'), 
     re_path(r'^swagger(?P<format>\.json|\.yml)$',schema_view.without_ui(cache_timeout=0),name='schema-json'),
     path('swagger/',schema_view.with_ui('swagger',cache_timeout=0),name='schema-swagger-ui'),
+    path('product/',ProductListAPIView.as_view(), name="product-list")
+    
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
+from .serializers import ProductSerializer
 
 def index(request):
     products = Product.objects.all()
@@ -20,3 +21,14 @@ class HelloWorldAPIView(APIView):
         return Response({'message': "Hello world"})
 
 
+
+class ProductListAPIView(APIView):
+    @swagger_auto_schema(
+        operation_description= "Get all product list",
+        responses={200: ProductSerializer(many=True)},
+    )
+    
+    def get(self, request, *args, **kwargs):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products,many=True)
+        return Response(serializer.data)
